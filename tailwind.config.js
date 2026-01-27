a9f18acab46c45ce750223d1549a130e305e145b
rainathecreatour-ops <rainathecreatour@gmail.com>
2026-01-27 14:06:31 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,5 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: ["./src/**/*.{js,jsx}", "./public/index.html"],
+  theme: { extend: {} },
+  plugins: []
+};
